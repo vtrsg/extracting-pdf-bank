ec77b7bb719582c9c105bf4a1b54d57fc7a38730
vtrsg <vittorioschmittg@gmail.com>
2023-08-18 16:01:04 -0300

    refact: modificando biblioteca por conta da falta de consistencia no tabula, com isso alterando alguns processos no projeto

--- index.py
+++ index.py
@@ -1,62 +1,88 @@
-
 import json
 import re
+import camelot
+
 from datetime import datetime
 from pathlib import Path
-
-import PyPDF2 as pdf
-import tabula
 
 file_path = "data\extrato-test-bb.pdf"
 path_destination = ""
 
-data_frames = tabula.io.read_pdf(file_path, pages="all", encoding="utf-8", multiple_tables= True, pandas_options={'header': None})
+# extraindo as tabelas com o camelot
+data_frames = camelot.read_pdf(file_path, pages="all", flavor="stream", strip_text='\n')
 
-list = []
+#variáveis importantes 
 sum = 0
 sum_credit = 0
 sum_debt = 0
 
+# Padrões de regex 
 date_pattern = re.compile(r"\d{2}/\d{2}/\d{4}")
-valor_pattern = re.compile(r'(\d{1,3}(?:\.\d{3})*,\d{2}) [CD]')
+value_pattern = re.compile(r'(\d{1,3}(?:\.\d{3})*,\d{2}) [CD]')
 
-for data_frame in data_frames:
-    data_frame = data_frame.iloc[:, [0, 3, 4, 5]]
-    data_frame.columns = ["data", "nome", "doc","valor"]
-    
-    data_list = data_frame.to_dict(orient="records")
-    
-    for data in data_list:       
-        data['data'] = str(data['data'])  
-        data['valor'] = str(data['valor'])  
+# extraindo colunas desnecessarias e renomeando as colunas das tabelas 
+def extract_columns(df):
+    if len(df.columns) >= 5:
+        if len(df.columns) == 5: 
+            df = df.drop(columns=[2, 4])
+        if len(df.columns) == 6: 
+            df = df.drop(columns=[1, 3, 5])
+        if len(df.columns) == 7: 
+            df = df.drop(columns=[1, 2, 4, 6])
         
-        if data['doc'] == "70":
-            list.append({'data': data['data'], 'nome': data['nome'], 'doc': data['doc'], 'valor': data['valor']})
-            if data['valor'][-1] == "D":
-                sum_debt -= float(data['valor'].replace('.', '').replace(',', '.').replace(' ', '').replace('C', '').replace('D', ''))
-            else:
-                sum_credit += float(data['valor'].replace('.', '').replace(',', '.').replace(' ', '').replace('C', '').replace('D', ''))
-        else:
-            doc_exists = any(item['doc'] == data['doc'] and item['valor'][-1] != data['valor'][-1] for item in list)
-            if doc_exists:
-                print(data['doc'])
-            if date_pattern.findall(data['data']) and valor_pattern.match(data['valor']) and not doc_exists:
-                
-                list.append({'data': data['data'], 'nome': data['nome'], 'doc': data['doc'], 'valor': data['valor']})
-                if data['valor'][-1] == "D":
-                    sum_debt -= float(data['valor'].replace('.', '').replace(',', '.').replace(' ', '').replace('C', '').replace('D', ''))
-                else:
-                    sum_credit += float(data['valor'].replace('.', '').replace(',', '.').replace(' ', '').replace('C', '').replace('D', ''))
+        df = df.iloc[:, [0, 1, 2]]
+        df.columns = ["data", "nome", "valor"]
 
+        return df
+
+# convertendo tabelas em dicionário    
+def convert_table(tables):
+    obj_list = []
+
+    for table in tables:
+        df = table.df
+        df = extract_columns(df)
+
+        if df is not None:
+            data_dict = df.to_dict(orient="records")
+            obj_list.extend(data_dict)
+    return obj_list
+
+# função para remover sinal de + das datas
+def remove_plus(date_str):
+    return date_str.rstrip('+')
+
+# função para ordenar o array pela data
+def sort_data(data_list):
+    sorted_list = sorted(data_list, key=lambda data: datetime.strptime(remove_plus(data['data']), "%d/%m/%Y").timestamp())
+    return sorted_list
+
+# lista de objetos iteráveis com os valores das tabelas para serem tratados
+data_list = convert_table(data_frames)
+
+# lista final para adicionar os elementos validos
+final_list = []
+
+# processo principal para extrair apenas os valores desejados e ja adicionar seu valor as respectivas variaveis
+for data in data_list:       
+    data['data'] = str(data['data'])  
+    data['valor'] = str(data['valor'])  
+    
+    if date_pattern.findall(data['data']) and value_pattern.match(data['valor']):            
+        final_list.append({'data': data['data'], 'nome': data['nome'], 'valor': data['valor']})
+        if data['valor'][-1] == "D":
+            sum_debt -= float(data['valor'].replace('.', '').replace(',', '.').replace(' ', '').replace('C', '').replace('D', ''))
+        else:
+            sum_credit += float(data['valor'].replace('.', '').replace(',', '.').replace(' ', '').replace('C', '').replace('D', ''))
+
+#somando valores de crédito, débito e total 
 sum_credit = round(sum_credit, 2)
 sum_debt = round(sum_debt, 2)
 sum = round(sum_credit + sum_debt, 2)
 
-def sort_data(data_list):
-    return sorted(data_list, key=lambda data: datetime.strptime(data['data'], "%d/%m/%Y").timestamp())
+final_list = sort_data(final_list)
+final_list.append({'total de créditos': sum_credit, 'total de débitos': sum_debt, 'total': sum})
 
-sorted_result = sort_data(list)
-sorted_result.append({'total de créditos': sum_credit, 'total de débitos': sum_debt, 'total': sum})
-
-result_json = json.dumps(sorted_result, ensure_ascii=False, indent=4)
+# criando json 
+result_json = json.dumps(final_list, ensure_ascii=False, indent=4)
 print(result_json)
